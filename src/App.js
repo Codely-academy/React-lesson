bafb12d9e6ea8426f30635a5dd4971724bf428b7
Codely-academy <136770740+Codely-academy@users.noreply.github.com>
2023-07-24 08:41:51 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import './App.css';
+import Box from './components/Box';
+const App = () => {
 
-function App() {
+  console.log("HELLOO APP.JS");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>HELLOO</h1>
+      <div className='container'>
+        <Box />
+        <Box />
+        <Box />
+      </div>
     </div>
   );
 }
