3d1171f5c5464e6060dc9526005eac3e4b0dd6b3
Codely-academy <136770740+Codely-academy@users.noreply.github.com>
2023-07-25 03:01:09 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
 import Box from './components/Box';
+import Friend from './components/Friend'
 const App = () => {
-
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
   console.log("HELLOO APP.JS");
-
+  const friends = [
+    {
+      name: 'Баясаа',
+      gender: 'Эр',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg'
+    },
+    {
+      name: 'Дэлгэрцэцэг',
+      gender: 'Эм',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg'
+    },
+    {
+      name: 'Шижир',
+      gender: 'Эр',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg'
+    },
+  ]
   return (
     <div>
       <h1>HELLOO</h1>
       <div className='container'>
-        <Box />
-        <Box />
-        <Box />
+        {
+          friends.map((friend) => {
+            return <Friend data={friend} />
+          })
+        }
       </div>
     </div>
   );
