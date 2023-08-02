7dfab52e2911f00bfee94291d630a9f2783a4d10
Bayasgalan Battulga <b.bayasgalan0912@gmail.com>
2023-08-02 12:18:23 +0800

    update

--- src/components/List.js
+++ src/components/List.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const List = (props) => {
     const { data } = props
+    // const fruits = ['Banana', 'Apple', 'Orange', 'Mango', 'Pineapple', 'Watermelon']
     return (
         <div>
-            {data.map((fruit) => <p>{fruit}</p>)}
+            {
+                data.map(
+                    (obj) => {
+                        return <p>{obj.email}</p>
+                    }
+                )
+            }
         </div>
     );
 }
--- src/components/ListFilter.js
+++ src/components/ListFilter.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react'
 import List from './List';
-
+import { users } from '../data/users';
 const ListFilter = () => {
     const [text, setText] = useState('');
     const onChange = (e) => {
         const text = e.target.value;
         setText(text)
     }
-    const fruits = ['Banana', 'Apple', 'Orange', 'Mango', 'Pineapple', 'Watermelon']
-    // App
+    // const fruits = ['Banana', 'Apple', 'Orange', 'Mango', 'Pineapple', 'Watermelon']
+
+    // App 
     // [Apple, Pineapple]
-    const filteredData = fruits.filter((fruit) => {
-        return fruit.toLowerCase().includes(text)
+    const filteredData = users.filter((user) => {
+        return user.email.includes(text)
     })
-    const listData = filteredData ? filteredData : fruits
+
+    const listData = filteredData ? filteredData : users
     return (
         <div>
             <input value={text} onChange={onChange} name='search' placeholder='search' />
@@ -23,3 +25,32 @@ const ListFilter = () => {
 }
 
 export default ListFilter;
+
+
+
+/*
+1. Input => "Z" текст бичлээ 
+
+2. <input value={text} onChange={onChange} name='search' placeholder='search' />
+ => onChange буюу өөрчлөлт гарлаа гэж мэдрээд onChange дээр холбосон 
+функцийг ажиллуулна. 
+
+3. onChange() функц ажиллана. Энэ функц нь гараас бичсэн текстийг аваад text гэсэн
+state -г шинэчилнэ. 
+const onChange = (e) => { 
+    // Гараас бичсэн текст-г text хувсагчид онооно
+        const text = e.target.value;
+        setText(text)
+    }
+
+
+4. State шинэчлэгдэх үед компонент доторхи код дахин нэг удаа ажиллана. Ингэхдээ "text"
+ state-н шинэчлэгдсэн утгаар ажиллана.
+const filteredData = fruits.filter((fruit) => {
+    return fruit.toLowerCase().includes(text)
+}) 
+//  filteredData => [];
+const listData = filteredData ? filteredData : fruits
+// listData => []
+
+*/
